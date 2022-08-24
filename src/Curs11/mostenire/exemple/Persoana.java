6386affc30870f98216b6594cda88b9613fd1ce9
bercii23 <bercean.dragos@yahoo.com>
2022-08-24 03:15:10 -0700

    cursuri

---
+++ src/Curs11/mostenire/exemple/Persoana.java
@@ -0,0 +1,11 @@
+package Curs11.mostenire.exemple;
+
+public class Persoana {
+    String name;
+    int age;
+
+    public Persoana(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
